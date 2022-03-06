4e3f67845c75f7096182144ac59faeaf49a01f8c
Damien Jacob <damienjacob@MBP-de-Damien.fritz.box>
2022-03-06 15:18:32 +0100

    ajout de la carte dans l'app

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 def recup_commune(code_postal):
     #addresse api
@@ -15,3 +16,45 @@ def recup_commune(code_postal):
     population = commune['population']
     
     return insee, ville, population
+
+
+def recup_list_stations(insee, ville):
+    #code de la ville
+    insee_str = str(insee)
+    #url pour la requete
+    station_url = f'https://hubeau.eaufrance.fr/api/v1/niveaux_nappes/stations?code_commune={insee_str}&format=json&size=20&pretty'
+    station_req = requests.get(station_url).json()
+    #list des stations piezometriques
+    stations = station_req['data']
+
+    print(f"il y a {len(stations)} station(s) à {ville}")
+    
+    return station_req, stations
+
+
+def station_max_mesure(stations):
+    mesure_max = 0
+    code_bss = ''
+    station = {}
+
+    for s in stations:
+        mesures = s['nb_mesures_piezo']
+        if mesures > mesure_max:
+            mesure_max = mesures
+            code_bss = s['code_bss']
+            station = s
+    
+    print(f"la station {code_bss} possède le plus de mesure avec {mesure_max} mesures.")
+    
+    return station
+
+
+def station_to_map(station):
+    cols = ['y', 'x']
+
+    df_station = pd.DataFrame(station, index=[0])
+    df_station = df_station[cols]
+    df_station.rename(columns={'x':'lon'}, inplace=True)
+    df_station.rename(columns={'y':'lat'}, inplace=True)
+    
+    return df_station
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import functions as f
 import streamlit as st
+import time 
 
 
 st.write("""
@@ -8,8 +9,21 @@ st.write("""
 Avec cette application web vous allez pouvoir extraire les niveaux piézométriques de plusieurs stations d'observations françaises
 """)
 
+#1 recupère le code postal 
 code_postal = st.text_input('CODE POSTAL', '67000')
 
+#2 recupere le code INSEE associe au code potal
 insee, ville, population = f.recup_commune(code_postal)
 
-st.write(insee, ville, population)
+#3 recupere la liste des stations situées dans la ville
+station_req, stations = f.recup_list_stations(insee, ville)
+
+#4 afficher les stations
+
+#5 choisir une stations
+station = f.station_max_mesure(stations)
+
+# affiche la station selectionnée
+station_loc = f.station_to_map(station)
+st.map(station_loc)
+
