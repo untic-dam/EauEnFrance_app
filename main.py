2afeb79f0c8df70fb4377fe74343a3be427c2b69
Damien Jacob <damienjacob@MBP-de-Damien.fritz.box>
2022-03-07 09:34:34 +0100

    ajout de la sidebar

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ st.write("""
 Avec cette application web vous allez pouvoir extraire les niveaux piézométriques de plusieurs stations d'observations françaises
 """)
 
+st.sidebar.header('Recherche')
+
 #1 recupère le code postal ou nom de ville
-user_request = st.text_input('CODE POSTAL ou NOM DE LA VILLE', 'Wolfisheim')
+user_request = st.sidebar.text_input('CODE POSTAL ou NOM DE LA VILLE', 'Wolfisheim')
 
 #2 recupere le code INSEE associe au code potal ou au nom de la ville
 insee, ville = f.recup_code_insee(user_request)
 
 #3 recupere la liste des stations situées dans la ville
-st.write("Si la ville séléctionner ne possède pas de station alors c'est Colmar qui est choisi par défaut")
 station_req, stations = f.recup_list_stations(insee, ville)
 
 #4 afficher les stations
@@ -26,9 +27,9 @@ station_req, stations = f.recup_list_stations(insee, ville)
 list_stations = f.extrait_list_stations_to_selectbox(stations)
 
 #5selection de la station
-station_lbl = st.selectbox(
-        'Selectionner la station',
-        list_stations)
+station_lbl = st.sidebar.selectbox(
+                'Selectionner la station',
+                list_stations)
 
 #extraction du code bss
 station_code = station_lbl.split(' | ')[0]
@@ -41,7 +42,7 @@ station_loc = f.station_to_map(station)
 st.map(station_loc)
 
 #7 recupère la chronique
-code_bss = st.text_input('code bss :', station['code_bss']) 
+code_bss = st.sidebar.text_input('code bss :', station['code_bss']) 
 chronique_req, chroniques = f.recup_chronique(code_bss)
 
 #8 Afficher la piezo
@@ -54,16 +55,16 @@ df_chronique_brut.to_csv('data.csv', index=False)
 df_chronique = pd.read_csv('data.csv', index_col='date_mesure', parse_dates=True)
 
 #plot
-attribut = st.selectbox(
-        "Selectionner l'attribut ",
-        ['niveau_nappe_eau', 'profondeur_nappe'])
-resample_size = st.selectbox(
-        "moyenne",
-        ['Y', '6M', 'M', 'W', 'D'])
+attribut = st.sidebar.selectbox(
+                "Selectionner l'attribut ",
+                ['niveau_nappe_eau', 'profondeur_nappe'])
+resample_size = st.sidebar.selectbox(
+                "moyenne",
+                ['Y', '6M', 'M', 'W', 'D'])
 fig = f.matplot_piezo(df_chronique, attribut, resample_size)
 st.pyplot(fig)
 
 #9 telecharger data
-st.markdown(f.filedownload(df_chronique, ville, code_bss), unsafe_allow_html=True)
+st.sidebar.markdown(f.filedownload(df_chronique, ville, code_bss), unsafe_allow_html=True)
 
 
