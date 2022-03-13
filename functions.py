3235bc5bf47746f24bc67f0f76b400a13c8d67fa
Damien Jacob <damienjacob@MBP-de-Damien.fritz.box>
2022-03-13 15:38:21 +0100

    texte telecharger fichier CSV traduit en Fr

--- functions.py
+++ functions.py
@@ -184,5 +184,5 @@ def filedownload(df, ville, code_bss):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
     fn = f'piezo_{ville}_{code_bss}.csv'.replace('/','_')
-    href = f'<a href="data:file/csv;base64,{b64}" download="{fn}">Download CSV File</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="{fn}">Telecharger au format CSV</a>'
     return href
